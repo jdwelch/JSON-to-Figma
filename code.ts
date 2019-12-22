e871259c6759e164ddf6ca20c2beeb2cbd73e31f
Pavel Laptev <pal@alteos.com>
2019-12-22 23:57:04 +0100

    wip 2

--- code.ts
+++ code.ts
@@ -1,32 +1,24 @@
-// This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+let getNodes = (selection, obj, name) => {
+  selection.map((item, i) => {
+    if (typeof obj[i] !== "undefined") {
+      // console.log(item.characters);
+      // item.characters = "sd";
+      // console.log(obj[i][name]);
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser enviroment (see documentation).
+      figma.loadFontAsync(item.fontName).then(() => {
+        item.characters = obj[i][name];
+      });
+    }
+  });
+};
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { height: 320 });
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  // if (msg.type === 'create-rectangles') {
-  //   const nodes: SceneNode[] = [];
-  //   for (let i = 0; i < msg.count; i++) {
-  //     const rect = figma.createRectangle();
-  //     rect.x = i * 150;
-  //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  //     figma.currentPage.appendChild(rect);
-  //     nodes.push(rect);
-  //   }
-  //   figma.currentPage.selection = nodes;
-  //   figma.viewport.scrollAndZoomIntoView(nodes);
+figma.ui.onmessage = (msg) => {
+  getNodes(figma.currentPage.selection, msg.obj, msg.buttonName);
+  // if (msg.type === "selected") {
+  //   // console.log(msg.obj);
+  //   getNodes(figma.currentPage.selection, msg.obj);
   // }
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  // figma.closePlugin();
 };
